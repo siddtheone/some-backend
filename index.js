64c9c0524b88e3a1661af0c6af82cba1cc79307a
siddtheone <45398971+siddtheone@users.noreply.github.com>
2022-09-03 14:12:41 +0530

    routing

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ http
     // res.write('Hello World!!'); //write a response to the client
     // res.end(); //end the response
 
-    res.writeHead(200, {
-      'Content-Type': 'application/json',
-    });
+    if (req.url === '/') {
+      fs.createReadStream('./index.html').pipe(res);
+    }
 
-    // fs.createReadStream('./index.html').pipe(res);
-    res.end(JSON.stringify({ name: 'Lorem' }));
+    if (req.url === '/api') {
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+      });
+
+      res.end(JSON.stringify({ name: 'Lorem' }));
+    }
   })
   .listen(4567); //the server object listens on port 8080
